c102c0fa6d29601af85598251a6a92eb72be3930
Suigetsu <lagmed.019@gmail.com>
2023-08-11 12:58:18 +0100

    to reset session

---
+++ CPP01/ex05/Harl.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlagrini <mlagrini@1337.student.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/10 19:41:06 by mlagrini          #+#    #+#             */
+/*   Updated: 2023/08/11 12:08:40 by mlagrini         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+
+# include <iostream>
+
+class	Harl
+{
+	private:
+		void	debug(void);
+		void	info(void);
+		void	warning(void);
+		void	error(void);
+	public:
+		Harl();
+		~Harl();
+		void	complain(std::string level);
+};
+
+#endif
