6f68f327803cf486d54b9c54b447889dca4841ea
Suigetsu <lagmed.019@gmail.com>
2023-08-20 16:28:30 +0100

    redoing CPP03

---
+++ CPP03RE/ex01/ClapTrap.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlagrini <mlagrini@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/20 13:43:02 by mlagrini          #+#    #+#             */
+/*   Updated: 2023/08/20 16:13:34 by mlagrini         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <iostream>
+
+class	ClapTrap
+{
+	protected:
+		std::string		name;
+		unsigned int	HP;
+		unsigned int	NP;
+		unsigned int	AD;
+	public:
+		ClapTrap();
+		ClapTrap(std::string name);
+		ClapTrap(const ClapTrap &obj);
+		~ClapTrap();
+		ClapTrap	&operator=(const ClapTrap &obj);
+		void		attack(const std::string &target);
+		void		takeDamage(unsigned int amount);
+		void		beRepaired(unsigned int amount);
+		void		showStats(void);
+};
+
+#endif
