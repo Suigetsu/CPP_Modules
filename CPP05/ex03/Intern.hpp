3d0b99627374b6ff8b7fea92e43e72572832f8c4
Suigetsu <lagmed.019@gmail.com>
2023-10-25 16:58:28 +0100

    some stuff are deleted

--- CPP05/ex03/Intern.hpp
+++ CPP05/ex03/Intern.hpp
@@ -6,7 +6,7 @@
 /*   By: mlagrini <mlagrini@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 17:15:16 by mlagrini          #+#    #+#             */
-/*   Updated: 2023/10/25 13:06:47 by mlagrini         ###   ########.fr       */
+/*   Updated: 2023/10/25 16:49:22 by mlagrini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,5 @@ class	Intern
 		~Intern();
 		AForm	*makeForm(std::string formName, std::string target);
 };
-AForm	*createRobotomy(std::string target);
-AForm	*createShrubbery(std::string target);
-AForm	*createPresidential(std::string target);
 
 #endif
