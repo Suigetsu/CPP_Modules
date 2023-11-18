fcf548514d3c694d087b033094b4e856a093b283
Suigetsu <lagmed.019@gmail.com>
2023-11-18 18:26:10 +0100

    CPP09 and ex00 setup

---
+++ CPP09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlagrini <mlagrini@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/18 17:14:25 by mlagrini          #+#    #+#             */
+/*   Updated: 2023/11/18 18:21:52 by mlagrini         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
+
+# include <iostream>
+# include <algorithm>
+# include <map>
+# include <iterator>
+
+class	BitcoinExchange
+{
+	private:
+		std::map<>
+};
+
+#endif
