7ffd0c7d5ba924db535cee2df5a19fbaa2ef00f0
Suigetsu <lagmed.019@gmail.com>
2023-11-19 18:19:52 +0100

    started parsing

--- CPP09/ex00/BitcoinExchange.hpp
+++ CPP09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: mlagrini <mlagrini@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 17:14:25 by mlagrini          #+#    #+#             */
-/*   Updated: 2023/11/18 18:21:52 by mlagrini         ###   ########.fr       */
+/*   Updated: 2023/11/19 18:19:23 by mlagrini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,21 @@
 class	BitcoinExchange
 {
 	private:
-		std::map<>
+		std::string dates;
+		double		prices;
+	public:
+		BitcoinExchange();
+		BitcoinExchange(std::string date, double price);
+		BitcoinExchange(const BitcoinExchange &obj);
+		BitcoinExchange	&operator=(const BitcoinExchange &obj);
+		~BitcoinExchange();
+		std::string	getDate();
+		double	getPrice();
 };
+
+	// if (strptime(s.c_str(), "%Y-%m-%d", &tm))
+	// 	std::cout << "valid date" << std::endl;
+	// else
+	// 	std::cout << "not valid date" << std::endl;
 
 #endif
--- CPP09/ex00/main.cpp
+++ CPP09/ex00/main.cpp
@@ -6,18 +6,35 @@
 /*   By: mlagrini <mlagrini@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 17:14:23 by mlagrini          #+#    #+#             */
-/*   Updated: 2023/11/18 18:02:11 by mlagrini         ###   ########.fr       */
+/*   Updated: 2023/11/19 18:19:15 by mlagrini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <sstream>
+#include <fstream>
+#include <ctime>
+#include "BitcoinExchange.hpp"
 
 int	main(int ac, char **av)
 {
-	if (ac != 2)
+	struct tm tm;
+	std::string s("2024-01-01");
+	std::string	str;
+	std::fstream	file;
+	std::map<std::string, BitcoinExchange> dataMap;
+	
+	
+	if (ac != 2 || strncmp(&av[1][strlen(av[1]) - 4], ".csv", 4))
 	{
 		std::cerr << "Error: could not open file." << std::endl;
 		return (1);
 	}
-	
+	file.open(av[1], std::ios::in);
+	if (!file.is_open())
+	{
+		std::cerr << "Error: could not open file." << std::endl;
+		return (1);
+	}
+	return (0);
 }
